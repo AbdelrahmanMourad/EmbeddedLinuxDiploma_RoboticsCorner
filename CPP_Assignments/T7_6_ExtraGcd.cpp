076b415fc346c787821091dfd4fdddec5bab7a6c
Abdelrahman Mourad <abdelrahmanmourad.am@gmail.com>
2025-05-16 11:45:34 +0300

    T7.6 : Find Greatest Common Factor

--- CPP_Assignments/T7_6_ExtraGcd.cpp
+++ CPP_Assignments/T7_6_ExtraGcd.cpp
@@ -2,11 +2,10 @@
 
     Task(7.6):
     ----------
-       Write a C++ function that find the Greatest Common Divisor (GCD) of two numbers.abort
+       Write a C++ function that find the Greatest Common Divisor (GCD) of two numbers.
 
-       The greatest common dvisor (GCD), also called the greatest common factor, of two numbers
-        is the largest number that divides them both.For instance, the greatest common factor 
-        of 20 and 15 is 5, since 5 divides both 20 and 15 and no larger number has this property.
+       The greatest common dvisor (GCD), also called the greatest common factor, of two numbers is the largest number that divides them both.
+        For instance, the greatest common factor of 20 and 15 is 5, since 5 divides both 20 and 15 and no larger number has this property.
 
     Example:
         Sample Output:
@@ -15,15 +14,56 @@
             The Greatest Common Divisor is: 5
 ___________________________________________________________________________________________________________*/
 
-
-
-
 // Include libraries.
 #include <iostream>
+
+int Find_GCF(int &x, int &y)
+{
+    // Check if 0 or -ve numbers.
+    if (x <= 1 || y <= 1)
+    {
+        std::cout << "Wrong input (-ve) or zero number" << std::endl;
+        return 0;
+    }
+
+    if (x >= y) // Check which one is larger
+    {
+        if (x % y == 0) // Check if one is GCF to the other.
+        {
+            return y; // Then the smaller is the FCF to each
+        }
+        else
+        {
+            for (int i = y; i >= 1; i--) // Check from the small number to 1.
+            {
+                if ((x % i == 0) && (y % i == 0))
+                    return i; // GCF.
+            }
+        }
+    }
+    else
+    {
+        if (y % x == 0) // Check if one is GCF to the other.
+        {
+            return y; // Then the smaller is the FCF to each
+        }
+        else
+        {
+            for (int i = x; i >= 1; i--) // Check from the small number to 1.
+            {
+                if ((x % i == 0) && (y % i == 0))
+                    return i; // GCF.
+            }
+        }
+    }
+}
 
 // Entry point.
 int main()
 {
+    int x, y, GCF;
+    std::cin >> x >> y;
+    std::cout << "GCF=\t" << Find_GCF(x, y) << std::endl;
 
     return 0;
 }
