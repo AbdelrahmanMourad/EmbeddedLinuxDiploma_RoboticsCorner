2b13bf96199a6567dbcc828c7111fd0545f796b7
Abdelrahman Mourad <abdelrahmanmourad.am@gmail.com>
2025-06-20 21:49:59 +0300

    Session 21 Design Patterns

---
+++ CPP_ٍSessionLabs/Session_21_DesignPatterns/lab7_Adaptor.cpp
@@ -0,0 +1,30 @@
+/*
+    Adaptor == providing interfaces for legace code
+*/
+
+// use case 1
+
+extern "C"  //  Means use c-style compiler => no name-mangling "no functions overloading"
+{
+#include <stdio.h>
+
+    // Legacy c-code.
+    void veryOldDraw()
+    {
+        printf("legacyDray\n");
+    }
+}
+
+
+
+/*
+
+---------------------------------------------------------------------------------
+to build:
+---------
+    gcc -c .\functions.c    
+            // this will generate .o file
+    g++ -o adaptor .\adaptor.cpp .\legace.cpp .\functions.o         
+            // Generate adaptor executable from all the other 3 files.
+---------------------------------------------------------------------------------
+*/
