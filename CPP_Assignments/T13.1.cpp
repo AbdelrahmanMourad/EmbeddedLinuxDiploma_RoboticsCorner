8407274b0a7f0d0230bc116defa1fde3fa574d61
Abdelrahman Mourad <abdelrahmanmourad.am@gmail.com>
2025-06-24 16:45:13 +0300

    Constructors initializer lists

--- CPP_Assignments/T13.1.cpp
+++ CPP_Assignments/T13.1.cpp
@@ -4,12 +4,12 @@
     ===============================
 
     - Requirements:
-        In this task, you will work with a base class PageObject and two derived classes: Page and Copy. 
+        In this task, you will work with a base class PageObject and two derived classes: Page and Copy.
         The goal is to create a basic structure for handling page-related objects and their
         operations, specifically for adding, removing, and deleting items from both Page and Copy objects.
 
     - Key Points:
-        ○   Base Class (PageObject): 
+        ○   Base Class (PageObject):
             This class has three virtual methods: addItem(), removeItem(),
             and deleteItem(), which provide a common interface for derived classes. It does not
             have any pure virtual methods, so it is not an abstract class, and you can instantiate it
@@ -27,7 +27,7 @@
                 PageObject to a page.
             •   Use the provided methods in both classes to manipulate PageObject items and
                 print relevant messages to the console when adding, removing, or deleting items.
-        ○   Class diagram: 
+        ○   Class diagram:
             use a UML tool (such as UMLet or draw.io) to create a class diagram for
             this structure. The class diagram should visually represent the inheritance hierarchy
             between the PageObject, Page, and Copy classes, including their methods.
@@ -43,13 +43,32 @@
 /*  -------------------------------------------------------------------
                         Functions Prototype
     ------------------------------------------------------------------- */
+class signal_list_t /*40k*/
+{
+    int y = 1;
+};
 
+class Example
+{
+private:
+    const int x_;
+    signal_list_t &&ref_x;
+
+public:
+    // Example() : x_{10}, ref_x{signal_list_t ref_x2}{}
+    // Example() {}
+    Example(int x, signal_list_t &&ref) : x_{x}, ref_x{ref}{}
+    void print(void) { std::cout << "x_ = " << x_ << std::endl; }
+};
 
 /*  -------------------------------------------------------------------
                         Entry Point
     ------------------------------------------------------------------- */
 int main()
 {
+    // signal_list_t s1;
+    Example example1(5, signal_list_t s1);
+    example1.print();
 
     return 0;
 }
@@ -57,13 +76,3 @@ int main()
 /*  -------------------------------------------------------------------
                         Functions implementation
     ------------------------------------------------------------------- */
-
-
-
-
-
-
-
-
-
-
