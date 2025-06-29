26aa47cecebd0c5bb5d504ca39fbb20cd596abeb
Abdelrahman Mourad <abdelrahmanmourad.am@gmail.com>
2025-06-29 11:46:53 +0300

    Task(15.2): Strategy Pattern

--- CPP_Assignments/T15.2_StrategyPattern.cpp
+++ CPP_Assignments/T15.2_StrategyPattern.cpp
@@ -7,13 +7,13 @@
     Requirements:
     -   Implement a simple Strategy Design Pattern for calculating vehicle speed based on
         different car brands.
-        
+
         1.  Abstract Interface:
             *   Define an interface IStrategy with a pure virtual method calculateSpeed().
                 This will act as a base for different strategies representing how vehicle
                 speed is determined.
         2.  Concrete Strategy Classes:
-            *   Create two classes "StrategyBMW" and "StrategyMini" that inherit from "IStrategy". 
+            *   Create two classes "StrategyBMW" and "StrategyMini" that inherit from "IStrategy".
                 Implement the "calculateSpeed()" method in each class, where:
                 _   StrategyBMW should print "BMW Strategy".
                 _   StrategyMini should print "Mini Cooper Strategy".
@@ -38,13 +38,53 @@ ________________________________________________________________________________
 /*  -------------------------------------------------------------------
                         Functions Prototype
     ------------------------------------------------------------------- */
+class IStrategy
+{
+public:
+    virtual void calculateSpeed() = 0;
+    IStrategy() { std::cout << "Constructor Called: IStrategy.\n"; }
+    ~IStrategy() { std::cout << "Destructor Called: IStrategy.\n"; }
+};
 
+class StrategyBMW : public IStrategy
+{
+public:
+    void calculateSpeed() override { std::cout << "BMW Strategy.\n"; }
+    StrategyBMW() { std::cout << "Constructor Called: StrategyBMW.\n"; }
+    ~StrategyBMW() { std::cout << "Destructor Called: StrategyBMW.\n"; }
+};
+
+class StrategyMini : public IStrategy
+{
+public:
+    void calculateSpeed() override { std::cout << "Mini Cooper Strategey.\n"; }
+    StrategyMini() { std::cout << "Constructor Called: StrategyMiniCooper.\n"; }
+    ~StrategyMini() { std::cout << "Destructor Called: StrategyMiniCooper.\n"; }
+};
+
+class Vehicle
+{
+private:
+    IStrategy *ptr_Istrategy_;
+
+public:
+    Vehicle(IStrategy *Ptr_Istrategy) : ptr_Istrategy_{Ptr_Istrategy} { std::cout << "Constructor Called: Vehicle.\n"; }
+    ~Vehicle() { std::cout << "Destructor Called: Vehicle.\n"; }
+    void VehicleSpeed() { ptr_Istrategy_->calculateSpeed(); }
+};
 
 /*  -------------------------------------------------------------------
                         Entry Point
     ------------------------------------------------------------------- */
 int main()
 {
+    StrategyBMW BMW1;
+    StrategyMini Mini1;
+
+    Vehicle V1{&BMW1};
+    Vehicle V2{&Mini1};
+
+    V1.VehicleSpeed(), V2.VehicleSpeed();
 
     return 0;
 }
