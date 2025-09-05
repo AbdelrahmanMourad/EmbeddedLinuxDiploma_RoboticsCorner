68fbe0b77eeadc0245d3a6936ed7b50a54381ebe
AbdelrahmanMourad <abdelrahmanmourad.am@gmail.com>
2025-09-05 16:17:51 +0300

    add Tasks

---
+++ COURSE/01_linux_administration/Task2_editors_piping.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+# Shebang
+
+# 1) Create 2 text files, 1 file using (nano / vim), 
+#       and the other using cp command.
+
+
+# 2) Edit the second file by adding any text to it.
+
+
+# 3) Use diff command to display the added text.
+
+
+# 4) Use grep to search for a pattern and display the line contains the pattern.
+
+
+# 5) Use piping to have the output from a command to be the input to
+#       another (I.e compile a C++ program and put the output to a file).
