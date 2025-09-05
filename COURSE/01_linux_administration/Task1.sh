e47cd50f84c32f8075762dd7315c4bd311896a87
AbdelrahmanMourad <abdelrahmanmourad.am@gmail.com>
2025-09-05 16:04:48 +0300

    task-1

---
+++ COURSE/01_linux_administration/Task1.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+# Shebang
+
+# 1) Create a directory: 
+mkdir my_project
+# 2) Navigate into it: 
+cd my_project
+# 3) Check your location: 
+pwd
+# 4) Create a sub-directory: 
+mkdir src
+# 5) List contents: 
+ls
+# 6) Navigate into the subdirectory: 
+cd src
+# 7) Go back to the parent directory: 
+cd ..
+# 8) Remove the subdirectory: 
+rmdir src
+# 9) Verify it's removed: 
+ls
